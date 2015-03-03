13ef5a68a261e28f9dcf0374d59c648790f70bce
Rusinov Anton <ovvshit@gmail.com>
2015-03-04 00:44:34 +0200

    js

--- kadastr.js
+++ kadastr.js
@@ -1,95 +1,97 @@
-//This is attempt to make prototypical inheritance by making prototype of Ukrainian land kadaster
+(function() {
+    //This is attempt to make prototypical inheritance by making prototype of Ukrainian land kadaster
 
-var registeredArray = [],
-    zemKadastr = function() {
-        this.name = 'Державний земельний кадастр';
+    var registeredArray = [],
+        zemKadastr = function() {
+            this.name = 'Державний земельний кадастр';
+        }
+
+    Object.prototype.zemDilyanka = function() {
+        var zemdil = this;
+        zemdil.pravoviPolojenniya = {
+
+        }
+        zemdil.obmejennya = {};
+
+        zemdil.roztashuv = {
+            oblast: '',
+            rayon: '',
+            nasPunkt: ''
+        };
+
+        zemdil.kadastrNomer = {};
+
+        zemdil.coords = {};
+        zemdil.ploscha = null;
+
+        zemdil.yakist = {};
+        zemdil.vlasnik = {
+            firstName: '',
+            lastName: '',
+            thirdName: '',
+            passport: {
+                seria: '',
+                nomer: null,
+                kimVidano: '',
+                dataVidachi: '',
+                propiska: {
+                    oblast: '',
+                    nasPunkt: '',
+                    vulitsya: '',
+                    budinok: null,
+                    kvartyra: null,
+                    korpus: null
+                }
+            },
+            identifikaciniiNomer: null,
+            derjAkt: {
+                nomer: null,
+                kolyVidano: '',
+                kimVidano: '',
+                ploscha: ''
+            }
+        };
+        zemdil.korystyvach = {
+            firstName: '',
+            lastName: '',
+            thirdName: '',
+            passport: {
+                seria: '',
+                nomer: 534633,
+                kimVidano: '',
+                dataVidachi: '',
+                propiska: {
+                    oblast: '',
+                    nasPunkt: '',
+                    vulitsya: '',
+                    budinok: 3,
+                    kvartyra: '',
+                    korpus: null
+                }
+            }
+        };
+
+        zemdil.addKadastrNum = function(zone, rayon, kvartal, dilyanka) {
+            zemdil.kadastrNomer.zone = zone;
+            zemdil.kadastrNomer.rayon = rayon;
+            zemdil.kadastrNomer.kvartal = kvartal;
+            zemdil.kadastrNomer.dilyanka = dilyanka;
+            zemdil.kadastrNomer.nomer = zone + ":" + rayon + ":" + kvartal + ":" + dilyanka;
+        };
+
+        zemdil.addCoord = function(tochka, x, y, z) {
+            zemdil.coords[tochka] = {};
+            zemdil.coords[tochka].x = x || null;
+            zemdil.coords[tochka].y = y || null;
+            zemdil.coords[tochka].z = z || null;
+        };
     }
 
-Object.prototype.zemDilyanka = function() {
-
-    this.cilPrizn = '';
-    this.obmejennya = {};
-
-    this.roztashuv = {
-        oblast: '',
-        rayon: '',
-        nasPunkt: ''
-    };
-
-    this.kadastrNomer = {
-    };
-
-    this.coords = {};
-    this.ploscha = null;
-
-    this.yakist = {        
-    };
-    this.vlasnik = {
-        firstName: '',
-        lastName: '',
-        thirdName: '',
-        passport: {
-            seria: '',
-            nomer: null,
-            kimVidano: '',
-            dataVidachi: '',
-            propiska: {
-                oblast: '',
-                nasPunkt: '',
-                vulitsya: '',
-                budinok: null,
-                kvartyra: null,
-                korpus: null
-            }
-        },
-        identifikaciniiNomer: null,
-        derjAkt: {
-            nomer: null,
-            kolyVidano: '',
-            kimVidano: '',
-            ploscha: ''
-        }
-    };
-    this.korystyvach = {
-        firstName: '',
-        lastName: '',
-        thirdName: '',
-        passport: {
-            seria: '',
-            nomer: 534633,
-            kimVidano: '',
-            dataVidachi: '',
-            propiska: {
-                oblast: '',
-                nasPunkt: '',
-                vulitsya: '',
-                budinok: 3,
-                kvartyra: '',
-                korpus: null
-            }
-        }
-    };
-
-    this.addKadastrNum = function(zone, rayon, kvartal, dilyanka) {
-        this.kadastrNomer.zone = zone;
-        this.kadastrNomer.rayon = rayon;
-        this.kadastrNomer.kvartal = kvartal;
-        this.kadastrNomer.dilyanka = dilyanka;
-        this.kadastrNomer.nomer = zone + ":" + rayon + ":" + kvartal + ":" + dilyanka;
-    };
-
-    this.addCoord = function(tochka, x, y, z) {
-        this.coords[tochka] = {};
-        this.coords[tochka].x = x || null;
-        this.coords[tochka].y = y || null;
-        this.coords[tochka].z = z || null;
-    };
-}
-function addNewLand(){
-    var zemDil = {};
-    zemDil.zemDilyanka();
-    registeredArray.push(zemDil)
-}
-
-
-console.log(registeredArray)
+    function addNewLand() {
+        var zemDil = {};
+        zemDil.zemDilyanka();
+        registeredArray.push(zemDil)
+    }
+    addNewLand()
+    console.log(registeredArray[0])
+})();
